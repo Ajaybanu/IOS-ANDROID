8b7b8fb614627111f72fc03e637ea4f5320a2d0f
Ajay banu <ajay@Ajays-MacBook-Air.local>
2024-10-07 18:34:30 +0530

    almost finish

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import VideoCard from "../components/VedioCard";
+import FormField from "../components/FormField";
+import CustomButton from "../components/CustomButton";
+import InfoBox from "../components/InfoBox";
+import Loader from "../components/Loader";
+import Trending from "../components/Trending";
+import SearchInput from "../components/SearchInput";
+import EmptyState from "../components/EmptyState";
+
+export {
+  VideoCard,
+  FormField,
+  CustomButton,
+  InfoBox,
+  Loader,
+  Trending,
+  SearchInput,
+  EmptyState,
+};
